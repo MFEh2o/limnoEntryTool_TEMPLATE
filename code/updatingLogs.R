474113161a1c84be2071c265d20bbf13e0eb395e
Kaija Gahm (cary) <kaijagahm@Kaijas-MacBook-Pro.local>
2021-05-14 14:14:11 -0400

    made updatingLogs.R a little prettier and clearer

--- code/updatingLogs.R
+++ code/updatingLogs.R
@@ -1,34 +1,23 @@
-# script to update in-season database files from limno datasheets
-# 2019-04-16
-# BLB
-# Edited by Kaija Gahm, October 2020
+# Script to update in-season database files from limno datasheets
+# Last updated by Kaija Gahm, May 2021
+# Make sure to read the README document in the limnoEntryTool_TEMPLATE repository for full setup instructions, and instructions on how to run this tool.
 
-# Libraries
+# Packages and setup ------------------------------------------------------
 library(here)
-
+options(warning.length = 3000L, error.length = 3000L) # so full errors/warnings will print
 # Load the updateLimno function from the limnoEntry.R script 
 source(here("code", "limnoEntry.R"))
 
-# Set variables
+# Set variables -----------------------------------------------------------
 dbdir <- here() # database is in the root project directory
-db <- "MFEdb_20200530.db" # name of the database file
+db <- "CHANGE THIS" # name of the database file you're using. Try to use one with a specific date to make the workflow clear. For example, "MFEdb_20200530.db"
 funcdir <- here("code") # folder where the functions are stored
-sampleSheetsDir <- here("sampleSheets2020")
-logFilesDir <- here("logFiles2020")
-options(warning.length = 3000L, error.length = 3000L)
+sampleSheetsDir <- here("sampleSheets")
+logFilesDir <- here("logFiles")
 
-# Update 
-updateLimno(dbdir = dbdir, db = db, sampleSheetsDir = sampleSheetsDir, 
-            logFilesDir = logFilesDir, funcdir = funcdir, force_siteID = T, 
-            force_lakeID = T, force_newProjectID = T, force_retiredProjectID = T, 
-            force_depth = T, force_profileORP = T)
-
-# Steps for updating the Github of the in-season database
-# Go to the Terminal tab in the console below
-# Execute the following commands:
-
-## bash
-## git add -A
-## git commit -m "added samples from 2019-MM-DD"
-## git push origin master
-
+# Run the tool ------------------------------------------------------------
+updateLimno(dbdir = dbdir, 
+            db = db, 
+            sampleSheetsDir = sampleSheetsDir, 
+            logFilesDir = logFilesDir, 
+            funcdir = funcdir)
